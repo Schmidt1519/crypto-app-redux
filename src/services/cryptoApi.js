26a3d222a5e0c281968769ff5e651185a8ddee81
unknown <schmidt1519@gmail.com>
2021-10-03 22:26:41 -0500

    Added redux provider to index.js. Created store.js for redux store. Created cryptoApi.js in services folder and added the Api headers from RapidAPI, a createRequest function, and a cryptoApi variable and named the reducerPath, provided baseQuery, and added endpoints with getCryptos endpoint. Created and exported hook for getCryptos query. Updated Homepage to call the getCryptos query, created variable to access the data object, and updated the values of each statistic.

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery, } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',   
+    'x-rapidapi-key': '28c522a0f3msha9fd2d76a8ccc11p1c64dejsn795dfc2036b2'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
