f45794f6d241c11ea7fba285cbed0cf659302574
unknown <schmidt1519@gmail.com>
2021-10-09 21:30:34 -0500

    Added Title of coin name and slug price. Added a Select dropdown to set the time period.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -60,9 +60,25 @@ const CryptoDetails = () => {
     // )
 
     return(
-        <div>
-            CrytpoDetails {coinId}
-        </div>
+        <Col className="coin-detail-container">
+            <Col className="coin-heading-container">
+                <Title level={2} className="coin-name">
+                    {cryptoDetails.name} ({cryptoDetails.slug}) Price    
+                </Title>
+                <p>
+                    {cryptoDetails.name} Live price in US dollars.
+                    View value statistics, market cap and supply.
+                </p>
+            </Col>
+            <Select
+                defaultValue="7d"
+                className="select-timeperiod"
+                placeholder="Select Time Period"
+                onChange={(value) => setTimePeriod(value)}
+            >
+
+            </Select>
+        </Col>
     )
 }
 
